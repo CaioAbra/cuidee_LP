bee5fc4184c8ab7add5b68b6313ee67c4b65effe
ojgabrielleal <joaogabrieel79@gmail.com>
2025-07-25 23:28:07 -0300

    fix: remove unnecessary width class from WhatsAppCarousel container

--- src/components/Carousel/WhatsAppCarousel.tsx
+++ src/components/Carousel/WhatsAppCarousel.tsx
@@ -15,7 +15,7 @@ const slides = [
 
 export default function WhatsAppCarousel() {
     return (
-        <div className="w-full max-w-[1216px] mx-auto px-4">
+        <div className="max-w-[1216px] mx-auto px-4">
             <Swiper
                 modules={[Autoplay, Pagination]}
                 loop={true}
