a8842d65e896e47f339d9e5b6472018504c4abf3
CaioAbra <caioabra41@gmail.com>
2025-08-06 21:56:18 -0300

    Desativa lint no build e mantém rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,28 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: "https://apiappdev.cuidee.com.br/:path*", // URL da API
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 Adicionado aqui
+  },
   async rewrites() {
     return [
       {
-        source: "/api/:path*", 
-        destination: "https://apiappdev.cuidee.com.br/:path*", // URL da API
+        source: "/api/:path*",
+        destination: "https://apiappdev.cuidee.com.br/:path*",
       },
     ];
   },
